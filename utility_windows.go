9c46c6ddecfac1c11dec62cc172cbb5d7bbb35d0
Jacob Santos <jacob@essential-elements.net>
2015-08-25 18:21:53 -0500

    Remove OS specific util files.

--- utility_windows.go
+++
@@ -1,9 +0,0 @@
-package com
-
-import syscall "golang.org/x/sys/windows"
-
-// GetInt32FromCall retrieves Int32 from syscall.
-func GetInt32FromCall(obj, method uintptr) int32 {
-	ret, _, _ := syscall.Syscall(method, 1, obj, 0, 0)
-	return int32(ret)
-}
