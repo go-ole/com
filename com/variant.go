f7f7248377285599f6c5109d3011c54657da8042
Jacob Santos <jacob@essential-elements.net>
2015-08-26 16:21:31 -0500

    Moved functions from api variant file to variant go file.

--- com/variant.go
+++ com/variant.go
@@ -1,9 +1,10 @@
-package variant
+package com
 
 import (
 	"time"
 	"unsafe"
 
+	api "github.com/go-ole/com/internal/variant"
 	"github.com/go-ole/com/safearray"
 	"github.com/go-ole/com/types"
 	"github.com/go-ole/types/idispatch"
@@ -100,4 +101,34 @@ func VariantByValueType(value interface{}) types.Variant {
 	}
 
 	return variant
+}
+
+// VariantInit initializes memory for variant objects.
+func VariantInit(v *types.Variant) error {
+	return api.VariantInit(v)
+}
+
+// VariantClear clears the memory of variant objects.
+func VariantClear(v *types.Variant) error {
+	return api.VariantClear(v)
+}
+
+// VariantChangeType changes variant type.
+func VariantChangeType(source *types.Variant, flags uint16, vt types.VariantType) *types.Variant {
+	return api.VariantChangeType(source, flags, vt)
+}
+
+// VariantChangeTypeEx changes variant type.
+func VariantChangeTypeEx(source *types.Variant, locale uint32, flags uint16, vt types.VariantType) *types.Variant {
+	return api.VariantChangeTypeEx(source, locale, flags, vt)
+}
+
+// VariantCopy copies variant.
+func VariantCopy(source *types.Variant) *types.Variant {
+	return api.VariantCopy(source)
+}
+
+// VariantCopyIndirect copies variant reference.
+func VariantCopyIndirect(source *types.Variant) *types.Variant {
+	return api.VariantCopyIndirect(source)
 }
