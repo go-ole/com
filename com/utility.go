fd770a131a7ed4c177a3f011a989ed3647331887
Jacob Santos <jacob@essential-elements.net>
2015-08-26 16:23:16 -0500

    Move utility functions to other utility file.

--- com/utility.go
+++ com/utility.go
@@ -4,6 +4,11 @@ import (
 	"syscall"
 	"unicode/utf16"
 	"unsafe"
+
+	api "github.com/go-ole/com/internal/util"
+	"github.com/go-ole/com/types"
+	"github.com/go-ole/types/idispatch"
+	"github.com/go-ole/types/iunknown"
 )
 
 // GetInt32FromCall retrieves Int32 from syscall.
@@ -87,4 +92,44 @@ func lpOleStrLen(p *uint16) (length int64) {
 func GetInt32FromCall(obj, method uintptr) int32 {
 	ret, _, _ := syscall.Syscall(method, 1, obj, 0, 0)
 	return int32(ret)
+}
+
+func SysAllocString(v string) *int16 {
+	return api.SysAllocString(v)
+}
+
+func SysAllocStringLen(v string) *int16 {
+	return api.SysAllocStringLen(v)
+}
+
+func SysFreeString(v *int16) error {
+	return api.SysFreeString(v)
+}
+
+func SysStringLen(v *int16) uint32 {
+	return api.SysStringLen(v)
+}
+
+func CreateStdDispatch(unk *iunknown.IUnknown, v uintptr, ptinfo *iunknown.IUnknown) (*idispatch.Dispatch, error) {
+	return api.CreateStdDispatch(unk, v, ptinfo)
+}
+
+func CreateDispTypeInfo(idata *INTERFACEDATA) (*iunknown.IUnknown, error) {
+	return api.CreateDispTypeInfo(idata)
+}
+
+func CopyMemory(dest, src unsafe.Pointer, length uint32) {
+	api.CopyMemory(dest, src, length)
+}
+
+func GetUserDefaultLocaleID() uint32 {
+	return api.GetUserDefaultLocaleID()
+}
+
+func GetMessage(msg *types.Msg, hwnd, MsgFilterMin, MsgFilterMax uint32) (int32, error) {
+	return api.GetMessage(msg, hwnd, MsgFilterMin, MsgFilterMax)
+}
+
+func DispatchMessage(msg *types.Msg) int32 {
+	return api.DispatchMessage(msg)
 }
